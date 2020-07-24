55e32ee2d171806857d38731a4ca1d5f90a4c095
Alisher <mcpeblocker@gmail.com>
2020-07-24 14:41:55 +0500

    added mongoose to server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,33 @@
+require('dotenv').config();
 import express = require("express");
-import authRoute from './routes/auth';
-
-const config: any = require("./config.json")
-
 const app: express.Application = express();
+const config: any = require("./config.json")
+import mongoose = require('mongoose');
+import winston = require('winston');
+import authRoute from './routes/auth';
+const logger: winston.Logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename:'./logs/index.log'})
+    ]
+});
+
+const mongodbURI: any = process.env.mongodbURI;
+
+mongoose.connect(mongodbURI,{
+    useCreateIndex:true,
+    useFindAndModify:true,
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+},(err:any) => {
+    if(err)
+        return logger.error('Cannot connect to the mongodb cloud!');
+});
 
 app.use('/api/auth',authRoute);
 
 app.get("/", (req: any, res: any) => {
-    res.send("/ main page");
+    res.send("/ main page");4
 })
 
 app.listen(config.port, () => {
